feb28e64a9f38f2b904be2ab8f53339e78cf9909
Ethan Young <ethan.young@tenovernine.com>
2015-07-21 12:06:34 -0600

    Add the gallery page for trips.

--- routes/trips.js
+++ routes/trips.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var tripRepository = require('../lib/trip-repository');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var viewModel = {
     titlePrefix: 'Trips',
-    isTripsActive: true,
+    isTripsActive: true
   };
-  res.render('trips', viewModel);
+  res.render('trips/all-trips', viewModel);
+});
+
+/* GET gallery page. */
+router.get('/:tripId', function(req, res, next) {
+  var tripId = req.params.tripId;
+
+  var viewModel = {
+    titlePrefix: 'Trips',
+    isTripsActive: true,
+    trip: tripRepository.getTrip(tripId)
+  };
+  res.render('trips/gallery', viewModel);
 });
 
 module.exports = router;
