c898cefe3967e52f5403984b69f7141acb9e62a7
Ethan Young <ethan.young@tenovernine.com>
2015-07-21 10:15:23 -0600

    Define the list of trips outside the jade file and loop through them.

--- routes/trips.js
+++ routes/trips.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var tripRepository = require('../lib/trip-repository');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var viewModel = {
     titlePrefix: 'Trips',
-    isTripsActive: true
+    isTripsActive: true,
+    trips: tripRepository.getAllTrips()
   };
   res.render('trips', viewModel);
 });
